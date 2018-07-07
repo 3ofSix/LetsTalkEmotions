7fdc8935c737e0d7f4a22dc8d93bdadee3500f23
3ofSix <garrett.vernon@gmail.com>
2018-07-07 13:23:31 +0100

    Quick Code tidy

--- app/src/main/java/com/garrettvernon/letstalkemotions/AddChild.java
+++ app/src/main/java/com/garrettvernon/letstalkemotions/AddChild.java
@@ -10,45 +10,42 @@ import android.view.View;
 import android.widget.EditText;
 
 public class AddChild extends AppCompatActivity {
-    private static final String TAG = "AddChildActivity";
-    private ChildViewModel mChildViewModel;
-    private Child mChild;
+            private static final String TAG = "AddChildActivity";
+            private ChildViewModel mChildViewModel;
+            private Child mChild;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_child);
-        mChildViewModel = ViewModelProviders.of(this).get(ChildViewModel.class);
-        mChild = new Child();
-        initOnClick();
-    }
-
-    private void initOnClick() {
-        final View addChild = findViewById(R.id.add_child);
-        addChild.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: clicked");
-                addChild();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_add_child);
+                mChildViewModel = ViewModelProviders.of(this).get(ChildViewModel.class);
+                mChild = new Child();
+                initOnClick();
             }
-        });
-    }
 
-    /**
-     * Called when user taps on 'Add Child'
-     */
-    public void addChild(){
-        EditText firstName = findViewById(R.id.firstName);
-        String firstNameString = firstName.getText().toString();
-        EditText lastName = findViewById(R.id.lastName);
-        String lastNameString = lastName.getText().toString();
-        mChild.setFirstName(firstNameString);
-        mChild.setLastName(lastNameString);
-        mChildViewModel.insert(mChild);
-        /*//Return to MainActivity
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);*/
-    }
+            private void initOnClick() {
+                final View addChildView = findViewById(R.id.add_child);
+                addChildView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d(TAG, "onClick: clicked");
+                        addChild();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            /**
+             * Called when user taps on 'Add Child'
+             */
+            public void addChild(){
+                EditText firstName = findViewById(R.id.firstName);
+                String firstNameString = firstName.getText().toString();
+                EditText lastName = findViewById(R.id.lastName);
+                String lastNameString = lastName.getText().toString();
+                mChild.setFirstName(firstNameString);
+                mChild.setLastName(lastNameString);
+                mChildViewModel.insert(mChild);
+            }
 }
